4f8ef4bdcb0eceb509d6152df12d4f1bf49cbd1f
Denis Lund <vasilenko9999@gmail.com>
2021-12-04 13:33:31 +0300

    Add project files.

---
+++ MyProject01.cpp
@@ -0,0 +1,49 @@
+// MyProject01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// Cледующая функция печатает
+// Вызывает функцию std::cout
+void print()
+{
+    // Потому что хотим использовать станлдартную бибилиотеку для печати в консоль и
+    // не использовать какие-либо другие библиотеки.
+    std::cout << "Puk :^3\n";
+}
+/*
+* Присваиваем значение переменным и печатаем "Puk!"
+*/
+
+int main()
+{ 
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 188;
+
+    int test2 = 1881;
+
+
+
+    int mult = x * y;
+    int random = 1515;
+
+        std::cout << random;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
